5ae63c61473f018af9ebd54a4cc9beb2df71fdd1
Jincheng Zhang <zhjch05@live.com>
2017-01-23 08:02:11 -0500

    animation with bugs

--- controller/viz.py
+++ controller/viz.py
@@ -1,5 +1,7 @@
 import matplotlib as mpl
-from matplotlib import pyplot
+#mpl.use('TKAgg')
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import numpy as np
 from model.dt import RoadMap
 import time
@@ -13,28 +15,38 @@ class Viz():
     def get2dArray(self):
         r = self.mapA.r
         c = self.mapA.c
-        rawMatrix = [[0 for x in range(c)] for y in range(r)]
+        self.rawMatrix = [[0 for x in range(c)] for y in range(r)]
         for i in range(self.mapA.r):
             for j in range(self.mapA.c):
                 if self.mapA.Matrix[i][j].isDisabled == 1:
-                    rawMatrix[i][j] = -5
+                    self.rawMatrix[i][j] = -5
                 elif self.mapA.Matrix[i][j].hasCar():
-                    rawMatrix[i][j] = 5
+                    self.rawMatrix[i][j] = 5
                 else:
-                    rawMatrix[i][j] = 0
-        return rawMatrix
+                    self.rawMatrix[i][j] = 0
+        return self.rawMatrix
+
+    def updatefig(self, *args):
+        self.t.tick()
+        self.rawMatrix = self.get2dArray()
+        self.img.set_data(self.rawMatrix)
+        return self.img
+
+
 
     def show(self):
-        self.t.tick()
-        rawMatrix = self.get2dArray()
+        self.rawMatrix = self.get2dArray()
         # make a color map of fixed colors
         cmap = mpl.colors.ListedColormap(['red','grey','black'])
         bounds=[-6,-2,2,6]
         norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
         # tell imshow about color map so that only set colors are used
-        img = pyplot.imshow(rawMatrix,interpolation='nearest', cmap = cmap,norm=norm)
-        pyplot.xticks(np.arange(0, 50, 1.0))
-        pyplot.yticks(np.arange(0, 9, 1.0))
+        self.img = plt.imshow(self.rawMatrix,interpolation='nearest', cmap = cmap,norm=norm, animated=True)
+        plt.xticks(np.arange(0, 50, 1.0))
+        plt.yticks(np.arange(0, 9, 1.0))
 
-        pyplot.grid(True,color='white',linewidth = 1.5 )
-        pyplot.show()
+        plt.grid(True,color='white',linewidth = 1.5 )
+        fig = plt.figure()
+
+        anim = animation.FuncAnimation(fig, self.updatefig, interval=50)
+        plt.show()
